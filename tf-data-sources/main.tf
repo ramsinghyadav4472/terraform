9816f8472d69f19524da22516a643f8badf85f61
Your New Name <ramsinghyadav4472@gmail.com>
2026-05-13 22:51:17 +0530

    Add AWS security group and VPC data sources with outputs

--- tf-data-sources/main.tf
+++ tf-data-sources/main.tf
@@ -19,7 +19,53 @@ data "aws_ami" "name" {
 output "aws_ami" {
   //value = data.aws_ami.name
   value = data.aws_ami.name.id
+  
+
 }
+
+#security group
+data "aws_security_group" "sg" {
+  tags = {
+    mywebserver = "http"
+  }
+}
+
+output "security_group" {
+    value = data.aws_security_group.sg
+    //value = data.aws_security_group.name
+  //value = data.aws_security_group.sg.id
+
+  
+}
+
+#VPC Id
+data "aws_vpc" "vpc" {
+  tags = {
+    ENV = "prod"
+    Name = "myvpc"
+  }
+}
+
+output "vpc_id" {
+    value = data.aws_vpc.vpc.id
+}
+
+#AZ
+data "aws_availability_zones" "az" {
+  state = "available"
+}
+
+ output "aws_zones" {
+        value = data.aws_availability_zones.az.names
+    }
+
+#to get the account details
+data "aws_caller_identity" "name" {
+}   
+output "account_id" {
+    value = data.aws_caller_identity.name.account_id
+}
+
 
 resource "aws_instance" "myserver" {
   ami = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
